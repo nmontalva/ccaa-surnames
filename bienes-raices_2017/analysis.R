ce1a4fff72b3e5a17e88476404fbb77d0668e342
nmontalva <nmontalva@gmail.com>
2017-10-29 02:49:52 -0300

    Functions to compute R, S, and A added to analysis.R

--- bienes-raices_2017/analysis.R
+++ bienes-raices_2017/analysis.R
@@ -75,29 +75,41 @@ dendrogram <- function(df) {
   dendroplot(df, clust_hedkin)
 }
 
-surname_diversity <- function(surnames_freq) {
-  data_surnames <- as.data.frame(surnames)
-  # disaggreagete frequencies
-  desag_surnames <- data.frame(
-    rep(data_surnames$Cognome, data_surnames$Freq),
-    rep(data_surnames$Population, data_surnames$Freq))
-  # put readable columns' names
-  colnames(desag_surnames) <- c("Cognome","Population")
-  
-  # a self-made function to compute "s"
-  fun_s <- function(x) length(unique(x$Cognome))
-  # get "s" of each population
-  ss <- by(desag_surnames, desag_surnames$Population, fun_s)
-  # get "n" of each population
-  ns <- by(desag_surnames, desag_surnames$Population, nrow)
-  
-  # this will make a table of n and s by population
-  S_table <- cbind(sapply(ns, I), sapply(ss, I))
-  colnames(S_table) <- c("n", "s")
-  
-  # get S
-  S_table <- as.data.frame(S_table)
-  S_table$S <- S_table$s / S_table$n
-  S_table
+
+##get S from "commoners" data
+fun_S <- function (commoners) {
+fun_s <- function(x){length(unique(x$surname_father))} # a self-made function to compute "s"
+ss <- by(commoners, commoners$community, fun_s) #get "s" of each population
+ns <- by(commoners, commoners$community, nrow) #get "n" of each population
+S_table <- cbind(sapply(ns,I),sapply(ss,I)) #This will make a table of n and s by population
+colnames(S_table) <- c("n","s") # To change colnames
+S_table <- as.data.frame(S_table)
+S_table$S <- S_table$s / S_table$n
+S_table
 }
+
+##get R from "commoners" data
+fun_R <- function (commoners) {
+fun_r <- function(x)sum(x$shares) # a self-made function to compute "r"
+rr <- by(commoners, commoners$community, fun_r) #get "r" of each population
+nr <- by(commoners, commoners$community, nrow) #get "n" of each population
+R_table <- cbind(sapply(nr,I),sapply(rr,I)) #This will make a table of n and a by population
+colnames(R_table) <- c("n","r") # To change colnames
+R_table <- as.data.frame(R_table)
+R_table$R <- R_table$r / R_table$n
+R_table
+}
+
+##get A from "commoners" data
+fun_A <- function (commoners) {
+fun_a <- function(x)sum(x$sex == "M") # a self-made function to compute "a"
+aa <- by(commoners, commoners$community, fun_a) #get "a" of each population
+na <- by(commoners, commoners$community, nrow) #get "n" of each population
+A_table <- cbind(sapply(na,I),sapply(aa,I)) #This will make a table of n and a by population
+colnames(A_table) <- c("n","a") # To change colnames
+A_table <- as.data.frame(A_table)
+A_table$A <- A_table$a / A_table$n
+A_table
+}
+
 
