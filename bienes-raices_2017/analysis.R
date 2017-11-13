dc34e76c9613156ae6e65c8f9b14419b82c89901
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2017-11-13 01:00:27 -0300

    traits are computed on groups defined by columns in group_by_col parameter (eg community, commune, province, region)
    * dendrogram functions also get a group_by_col parameter

--- bienes-raices_2017/analysis.R
+++ bienes-raices_2017/analysis.R
@@ -3,123 +3,140 @@ library(tidyverse)
 library(Biodem)
 library(ggdendro)
 library(reldist)
-# library(ade4)
+library(ade4)
 
 commoners_csv <- "commoners.csv"
 col_names <- c("community", "right_id",
                "firstname", "surname",
-               "shares", "commune",
+               "rights", "shares",
+               "commune", "province", "region",
                "firstname1", "firstname2",
                "surname_father", "surname_mother")
 read_commoners_csv <- function(filename=commoners_csv) {
   read_csv(commoners_csv,
            skip=1,
            col_names=col_names,
-           col_types="ciccdccccc")
+           col_types="ciccddccccccc")
 }
 
-stats_communities <- function(commoners) {
-  commoners %>%
-    group_by(region, province, commune, community) %>%
+traits <- function(commoners, col="community") {
+  args <- as.list(c(NA, col))
+  args[[1]] <- commoners
+  do.call(group_by_, args) %>%
     summarise(N=n(),
               S=n_distinct(surname_father)/N,
-              R=mean(shares),
-              G=gini(shares),
+              R=mean(rights),
+              G=gini(shares), # gini(rights),
               A=mean(sex == "M"))
 }
 
-dendroplot <- function(hc, sc, save_as=NULL) {
+dendroplot <- function(hc, save_as=NULL,
+                       group_by_col="community") {
   # generate dendrogram from hclust data
   hcd <- dendro_data(hc, type="rectangle")
   # get rid of those factors
   hcd$labels$label <- as.character(hcd$labels$label)
+  # traits
+  container <- if (group_by_col == "community") "commune"
+  else if (group_by_col == "commune") "province"
+  else if (group_by_col == "province") "region"
+  tc <- traits(commoners, c(group_by_col, container))
   # vectors to obtain commune, S, R, G, A of communities
-  vector_of <- function(col) {
-    v <- sc[[col]]
-    names(v) <- sc$community
+  vector_of <- function(target_col) {
+    v <- tc[[target_col]]
+    names(v) <- tc[[group_by_col]]
     v
   }
-  # province <- vector_of("province")
-  commune <- vector_of("commune")
+  location <- if (!is.null(container))
+    vector_of(container)
   N <- vector_of("N")
   S <- vector_of("S")
   R <- vector_of("R")
   G <- vector_of("G")
   A <- vector_of("A")
+  # useful coordinates
+  lastrow <- nrow(hcd$labels)
+  x0 <- hcd$labels$x[[lastrow]]
+  y0 <- hcd$labels$y[[lastrow]]
+  x1 <- x0 + 1 + 0.5 * lastrow / 170
+  ydiff <- if (is.null(container)) 0.4 else 0
   # output to pdf
-  if (!is.null(save_as))
-    pdf(save_as, width=12, height=40)
+  if (!is.null(save_as)) {
+    pdf(save_as,
+        width=8 + 4 * lastrow / 170,
+        height=1 + 40 * lastrow / 170)
+  }
+  size <- function(xs) {
+    xs * 1.3 / max(xs) + (1.7 + lastrow / 170)
+  }
   # plot
   p <- ggplot() +
     geom_segment(data=segment(hcd),
                  aes(x=x, y=y, xend=xend, yend=yend)) +
+    # because of the coord_flip at the end, x and y are flipped
     geom_text(data=label(hcd),
               aes(x=x, y=y, label=label, hjust=0),
               nudge_y=0.01,
               size=3) +
-    annotate("text", y=-0.30, x=172.5,
-             label="Community", fontface="bold", size=4) +
-    annotate("text", y=-1.30, x=172.5,
-             label="Commune", fontface="bold", size=4) +
-    annotate("text", y=-1.64, x=172.5,
+    annotate("text", x=x1, y=y0-0.215, # y=y0-0.30,
+             label=str_to_title(group_by_col),
+             fontface="bold", size=4) +
+    (if (!is.null(container))
+      annotate("text", x=x1, y=y0-1.265, # y=y0-1.30,
+               label=str_to_title(container),
+               fontface="bold", size=4)) +
+    annotate("text", x=x1, y=y0-1.64+ydiff,
              label="#", fontface="bold", size=4) +
-    annotate("text", y=-1.86, x=172.5,
+    annotate("text", x=x1, y=y0-1.86+ydiff,
              label="S", fontface="bold", size=4) +
-    annotate("text", y=-2.06, x=172.5,
+    annotate("text", x=x1, y=y0-2.06+ydiff,
              label="R", fontface="bold", size=4) +
-    annotate("text", y=-2.26, x=172.5,
+    annotate("text", x=x1, y=y0-2.26+ydiff,
              label="G", fontface="bold", size=4) +
-    annotate("text", y=-2.46, x=172.5,
+    annotate("text", x=x1, y=y0-2.46+ydiff,
              label="A", fontface="bold", size=4) +
-    geom_text(data=label(hcd),
-              aes(x=x, y=y, hjust=0,
-                  label=commune[label],
-                  colour=commune[label]),
-              nudge_y=1.1,
-              size=3,
-              show.legend=FALSE) +
+    (if (!is.null(container))
+      geom_text(data=label(hcd),
+                aes(x=x, y=y, hjust=0,
+                    label=location[label],
+                    colour=location[label]),
+                nudge_y=1.1,
+                size=3,
+                show.legend=FALSE)) +
     geom_text(data=label(hcd),
               aes(x=x, y=y,
                   label=N[label],
-                  size=N[label]*1.3/max(N[label])+2.7),
-              nudge_y=1.6,
+                  size=size(N[label])),
+              nudge_y=1.6-ydiff,
               hjust=0) +
     geom_text(data=label(hcd),
               aes(x=x, y=y,
                   label=prettyNum(S[label], digits=3),
-                  size=S[label]*1.3/max(S[label])+2.7),
-              nudge_y=1.8,
+                  size=size(S[label])),
+              nudge_y=1.8-ydiff,
               hjust=0) +
     geom_text(data=label(hcd),
               aes(x=x, y=y,
                   label=prettyNum(R[label], digits=3),
-                  size=R[label]*1.3/max(R[label])+2.7),
-              nudge_y=2.0,
+                  size=size(R[label])),
+              nudge_y=2.0-ydiff,
               hjust=0) +
     geom_text(data=label(hcd),
               aes(x=x, y=y,
                   label=prettyNum(G[label], digits=3),
-                  size=G[label]*1.3/max(G[label])+2.7),
-              nudge_y=2.2,
+                  size=size(G[label])),
+              nudge_y=2.2-ydiff,
               hjust=0) +
     geom_text(data=label(hcd),
               aes(x=x, y=y,
                   label=prettyNum(A[label], digits=3),
-                  size=A[label]*1.3/max(A[label])+2.7),
-              nudge_y=2.4,
+                  size=size(A[label])),
+              nudge_y=2.4-ydiff,
               hjust=0) +
     scale_size_identity() +
-    # scale_colour_manual(values=colours) +
-    # scale_colour_brewer(palette="Paired") +
-    # scale_colour_discrete(l=50) +
-    coord_flip() + scale_y_reverse(expand=c(0, 0.6)) +
-    theme(axis.line=element_blank(),
-          axis.ticks=element_blank(),
-          axis.text=element_blank(),
-          axis.title=element_blank(),
-          panel.background=element_rect(fill="white"),
-          panel.grid=element_blank()) +
-    labs(y="")
+    coord_flip() +
+    scale_y_reverse(expand=c(0, 0.6)) +
+    theme_dendro()
   # save plot
   if (!is.null(save_as)) {
     # ggsave(save_as, width=12, height=60, units="cm")
@@ -128,10 +145,11 @@ dendroplot <- function(hc, sc, save_as=NULL) {
   } else p
 }
 
-surname_distance_matrix <- function(commoners) {
+surname_distance_matrix <- function(commoners,
+                                    group_by_col="community") {
   # cross tabulate
   surnames_freq <- table(commoners$surname_father,
-                         commoners$community)
+                         commoners[[group_by_col]])
   # generates Hedrick (1971) kinship matrix
   # there are other methods (i.e. lasker, uri)
   hedkin <- hedrick(surnames_freq)
@@ -142,32 +160,34 @@ surname_distance_matrix <- function(commoners) {
 
 hclust_default_method <- "complete"
 surname_clustering <- function(commoners,
-                               method=hclust_default_method) {
-  surname_distance_matrix(commoners) %>%
+                               hclust_method=hclust_default_method,
+                               group_by_col="community") {
+  commoners %>%
+    surname_distance_matrix(group_by_col) %>%
     # hierarchical clustering of the distance matrix
-    hclust(method=method)
+    hclust(method=hclust_method)
 }
 
 surname_dendrogram <- function(commoners, save_as=NULL,
-                               method=hclust_default_method) {
-  hc <- surname_clustering(commoners, method)
-  sc <- stats_communities(commoners)
-  dendroplot(hc, sc, save_as=save_as)
+                               hclust_method=hclust_default_method,
+                               group_by_col="community") {
+  hc <- surname_clustering(commoners, hclust_method, group_by_col)
+  dendroplot(hc, save_as, group_by_col)
 }
 
 k_histogram <- function(commoners,
                         k,
                         save_as=NULL,
                         hclust_method=hclust_default_method) {
-  sc <- stats_communities(commoners)
+  tc <- traits_communities(commoners)
   sur_clust <- surname_clustering(commoners, hclust_method)
   kgroups <- cutree(sur_clust, k=k)
-  sc <- tibble(community=names(kgroups),
+  tc <- tibble(community=names(kgroups),
                branch=as.character(kgroups)) %>%
-    right_join(sc, by="community")
+    right_join(tc, by="community")
   if (!is.null(save_as))
     pdf(save_as, width=5, height=4)
-  p <- ggplot(data=sc) +
+  p <- ggplot(data=tc) +
     geom_histogram(aes(x=G, y=..density.., fill=branch),
                    bins=10, alpha=1/k,
                    position="identity", boundary=0)
@@ -178,13 +198,14 @@ k_histogram <- function(commoners,
   } else p
 }
 
-# compare surnames matrix with G matrix
-# compare_dist <- function(commoners) {
-#   sc <- stats_communities(commoners)
-#   G_dist <- dist(sc$G)
-#   hedkin_dist <- surname_distance_matrix(commoners)
-#   # R: I don't understand why one would use quasieuclid or not
-#   mantel.randtest(hedkin_dist, # quasieuclid(hedkin_dist),
-#                   G_dist, nrepet=9999)
-# }
+# compare surnames matrix with ,S, A, R, or G matrices
+compare_dist <- function(commoners, trait, dist_method = "euclid") {
+  tc <- traits_communities(commoners)
+  trait_dist <- dist(tc[[trait]], dist_method)
+  hedkin_dist <- surname_distance_matrix(commoners)
+  # R: I don't understand why one would use quasieuclid or not
+  mantel.randtest(hedkin_dist, # quasieuclid(hedkin_dist),
+                  trait_dist,
+                  nrepet=99999)
+}
 
