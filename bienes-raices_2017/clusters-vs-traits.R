97d2e95e6f467839e826d33d278e978e86acefce
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2019-06-22 01:43:26 -0400

    obtain p-values from anova only if possible.
    * replaced 'cluster' by 'clade'

--- bienes-raices_2017/clusters-vs-traits.R
+++ bienes-raices_2017/clusters-vs-traits.R
@@ -1,13 +1,12 @@
 library(tidyverse)
-# library(ggpubr) # for ggboxplot
 
-k_clusters <- function(commoners,
-                       k,
-                       hclust_method=hclust_default_method) {
+k_clades <- function(commoners,
+                     k,
+                     hclust_method=hclust_default_method) {
   sur_clust <- surname_clustering(commoners, hclust_method)
   kgroups <- cutree(sur_clust, k=k)
   tibble(community=names(kgroups),
-         branch=as.character(kgroups))
+         branch=kgroups)
 }
 
 k_histogram <- function(commoners,
@@ -16,7 +15,7 @@ k_histogram <- function(commoners,
                         save_as=NULL,
                         hclust_method=hclust_default_method) {
   tc <- traits(commoners) %>%
-    left_join(k_clusters(commoners, k, hclust_method),
+    left_join(k_clades(commoners, k, hclust_method),
               by="community")
   if (!is.null(save_as))
     pdf(save_as, width=5, height=4)
@@ -30,43 +29,47 @@ k_histogram <- function(commoners,
   } else p
 }
 
-# anova of a trait by clusters
-clusters_pval <- function(commoners,
-                          trait,
-                          ks=seq(2,169), # vector with the number of groups in which the tree is split
-                          hclust_method=hclust_default_method) {
-                          # hclust_method="ward.D2") {
+# anova of a trait by clades
+# ks is a vector with the number of groups in which the tree is split
+clades_pval <- function(commoners,
+                        trait,
+                        ks=seq(2,169),
+                        hclust_method=hclust_default_method) {
+                        # hclust_method="ward.D2") {
   ts <- traits(commoners)
   ks %>% map_dfr(function(k) {
-    tks <- left_join(ts, k_clusters(commoners, k, hclust_method),
+    tks <- left_join(ts, k_clades(commoners, k, hclust_method),
                      by="community")
-    clusters <- tks %>% group_by(branch) %>%
-      summarise(size=n())
-    # TODO: make code parametric on trait
-    # gini_mean=mean(G, na.rm=TRUE),
-    # gini_sd=sd(G, na.rm=TRUE))
+    # clades <- tks %>% group_by(branch) %>%
+    #   summarise(size=n(),
+    #             trait_mean=mean(!!sym(trait), na.rm=TRUE),
+    #             trait_sd=sd(!!sym(trait), na.rm=TRUE))
+    # size_mean <- mean(clades$size)
+    # size_sd <- sd(clades$size)
+    anova <- summary(aov(tks[[trait]] ~ tks$branch))[[1]]
+    pval_aov <- if ("Pr(>F)" %in% colnames(anova))
+      anova[["Pr(>F)"]][[1]]
+    else 0
+    # kw <- kruskal.test(tks[[trait]] ~ as.factor(tks$branch))
+    kw <- kruskal.test(tks[[trait]] ~ tks$branch)
     tibble(
-      num_clusters=k,
-      # plot=ggboxplot(tck, x="branch", y=trait, xlab="Cluster", ylab=trait),
-      # anova=summary(aov(tks[[trait]] ~ tks$branch)),
-      pval_aov=summary(aov(tks[[trait]] ~ tks$branch))[[1]][["Pr(>F)"]][[1]],
-      pval_kw=kruskal.test(tks[[trait]] ~ as.factor(tks$branch))$p.value,
-      n=mean(clusters$size),
-      sd=sd(clusters$size))
+      num_clades=k,
+      pval_aov=pval_aov,
+      pval_kw=kw$p.value)
   })
 }
 
 plot_pvalues_kw <- function(pvalues) {
-  ggplot(pvalues, aes(num_clusters, pval_kw)) +
+  ggplot(pvalues, aes(num_clades, pval_kw)) +
     geom_line() +
-    scale_x_continuous("Number of clusters") +
+    scale_x_continuous("Number of clades") +
     scale_y_continuous("Kruskal-Wallis p-value")
 }
 
 plot_pvalues_anova <- function(pvalues) {
-  ggplot(pvalues, aes(num_clusters, pval_aov)) +
+  ggplot(pvalues, aes(num_clades, pval_aov)) +
     geom_line() +
-    scale_x_continuous("Number of clusters") +
+    scale_x_continuous("Number of clades") +
     scale_y_continuous("ANOVA p-value")
 }
 
@@ -74,10 +77,9 @@ colourbar_pvalues <- function(pvalues) {
   ggplot(pvalues, aes(1, 1, fill=pval_aov)) +
     geom_bar(stat="identity") +
     scale_x_discrete("", expand=expand_scale()) +
-    scale_y_continuous("Number of clusters", expand=expand_scale()) +
+    scale_y_continuous("Number of clades", expand=expand_scale()) +
     scale_fill_viridis_c(option = "plasma", direction=-1) +
-    guides(fill = guide_colourbar(title="p-value",
-                                  barwidth=0.5, barheight=10)) +
+    guides(fill = guide_colourbar(title = "p-value")) +
     coord_flip()
 }
 
