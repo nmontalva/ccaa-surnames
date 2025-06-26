e7407e1f6fcfb39addd8587120ef215fc1de986d
Nicolas <nmontalva@gmail.com>
2025-06-25 22:54:48 -0400

    planning tailored results for evolutionary models

---
+++ models_NM/v01/tailored_results.R
@@ -0,0 +1,17 @@
+# Selected results
+
+## Idea:
+# 1. After running evolutionary analysis.R
+# 2. Pick the resulting object (i.e. "results")
+# 3. Use it to build tailored outputs.
+
+## A table to compare models
+# Compare BM, single OU, multi OU
+# For both G and M
+# Include relevant numbers such as:
+# - AIC
+# - What else?
+
+## Nicer trees
+# Better colours
+#
