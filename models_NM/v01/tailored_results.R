9add676c2191e89fd05c5e870596fe144b1842ff
Nicolas <nmontalva@gmail.com>
2025-06-27 13:31:43 -0400

    extended ideas on tailored results

--- models_NM/v01/tailored_results.R
+++ models_NM/v01/tailored_results.R
@@ -14,4 +14,17 @@
 
 ## Nicer trees
 # Better colours
-#
+# I don't like the colours
+# Titles lack the pointer to the trait name. Anyway, I may get rid of titles altogether
+# Remove "NA" from legend
+# Considering (not sure) to add alpha besides theta
+# The numeric scale at the bottom is meaningless
+# I think it needs thicker lines
+# For some reason, the smaller display sinked to RStudio visual device looks better thant the sinked output. No idea why.
+
+## Regimes
+# Summaries already have thetha and logit(theta).
+# I think we may also need alpha and other numbers. Maybe checkout what papers normally report.
+# Curiously, it seems like ´$parameters´ is empty (return NAs)
+# Read surface documentation to figure out what are does values stored at n_regimes: k, kprime, deltak, etc.
+# Regimes dist (plot) is nice, but that should just go on a table (?)
